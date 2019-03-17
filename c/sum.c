86b5fe4fbbfff44fd2ea271804bb59b74da260f4
Korovsky <sestra.che@gmail.com>
2019-03-17 16:32:16 +0300

    Added programs for calculating sum in c and js

---
+++ c/sum.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main() {
+int sum(int a, int b) {
+    return a + b;
+}
+int our_sum = sum(2, 3);
+printf("our sum is %d", our_sum);
+}
---
+++ js/sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b
+}
+
+our_sum = sum(3, 5)
+console.log(our_sum)
